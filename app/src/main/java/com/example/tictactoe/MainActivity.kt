324fb7bf5858c72ed72e0aacf1c1703b5c4d16b7
Iulia Severin <iulia.severin12@gmail.com>
2024-01-21 16:36:54 +0200

    Header implemented showing who's turn it is

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.tictactoe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.tictactoe.ui.theme.TictactoeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +35,71 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //wip
+                    TTTScreen()
                 }
             }
         }
     }
+}
+
+@Composable
+fun TTTScreen() {
+    //true - player's turn, false - AI's turn
+    val playerTurn by remember { mutableStateOf(true) }
+
+    //true - player move, false - AI move, null - no move
+    val moves = remember {
+        mutableStateListOf<Boolean?>(
+            true,
+            null,
+            false,
+            null,
+            true,
+            false,
+            null,
+            null,
+            null
+        )
+    }
+    Header(playerTurn)
+}
+
+@Composable
+fun Header(playerTurn: Boolean) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        val playerBoxColor = if (playerTurn) Color.Blue else Color.LightGray
+        val aiBoxColor = if (playerTurn) Color.LightGray else Color.Red
+
+        Box(
+            modifier = Modifier
+                .width(100.dp)
+                .background(playerBoxColor)
+        ) {
+            Text(
+                text = "Player", modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        Spacer(modifier = Modifier.width(50.dp))
+        Box(
+            modifier = Modifier
+                .width(100.dp)
+                .background(aiBoxColor)
+        ) {
+            Text(
+                text = "AI", modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.Center)
+            )
+        }
+    }
+}
+
+@Composable
+fun Board() {
+
 }
